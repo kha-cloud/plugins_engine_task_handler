cfa9bf904ce46e8f02e656a431fdc6f4d48ab758
Khalil ben zineb <khalil.benzineb@gmail.com>
2024-11-05 18:18:31 +0100

    11/05/24

--- master.js
+++ master.js
@@ -50,94 +50,102 @@ const execute_PETH_Master_runTask = async (data) => {
 }
 
 const runTask = async (taskMetaData) => {
-  /* taskMetaData : {
-    runAndWait, // Boolean
-    taskKey,
-    runId,
-    taskCodeUpdateCacheKey,
-    taskVersion.
-    // taskChunksUrls: [
-    //   {
-    //     url,
+  try {
+    /* taskMetaData : {
+      runAndWait, // Boolean
+      taskKey,
+      runId,
+      taskCodeUpdateCacheKey,
+      taskVersion.
+      // taskChunksUrls: [
+      //   {
+      //     url,
+      //   }
+      // ],
+      apiData: {
+        route, // EXP: "cyberocean.net"
+        token,
+        pluginKey,
+      },
+      taskConfig,
+    }*/
+  
+    await utils.init(taskMetaData);
+  
+    // // Check the cache if the task's code got an update
+    // const taskArchiveFolder = `${archiveFolder}/${taskMetaData.taskKey}`;
+    // var taskCacheData = await utils.loadJsonFile(taskCacheFilePath, {
+    //   taskCodeUpdateCacheKey: null,
+    //   tarFiles: [],
+    // });
+  
+    // if(taskMetaData.taskCodeUpdateCacheKey !== taskCacheData.taskCodeUpdateCacheKey) {
+    //   // Get the Task's taskChunksUrls
+    //   const taskChunksUrls = await utils.$dataCaller(
+    //     "get",
+    //     "/api/get_task_chunks_urls/" + taskMetaData.apiData.pluginKey + "/" + taskMetaData.taskKey
+    //   );
+      
+    //   // Download the task's code as a TAR file and set the new version locally
+    //   const downloadPromises = [];
+    //   const tarFiles = [];
+    //   for(let i = 0; i < taskChunksUrls.length; i++) {
+    //     const taskChunkUrl = taskChunksUrls[i];
+    //     const taskChunkPath = `${taskArchiveFolder}/${taskChunkUrl.name}.tar`;
+    //     tarFiles.push(taskChunkPath);
+    //     downloadPromises.push(utils.downloadFileToPath(taskChunkUrl.url, taskChunkPath));
     //   }
-    // ],
-    apiData: {
-      route, // EXP: "cyberocean.net"
-      token,
-      pluginKey,
-    },
-    taskConfig,
-  }*/
-
-  await utils.init(taskMetaData);
-
-  // // Check the cache if the task's code got an update
-  // const taskArchiveFolder = `${archiveFolder}/${taskMetaData.taskKey}`;
-  // var taskCacheData = await utils.loadJsonFile(taskCacheFilePath, {
-  //   taskCodeUpdateCacheKey: null,
-  //   tarFiles: [],
-  // });
-
-  // if(taskMetaData.taskCodeUpdateCacheKey !== taskCacheData.taskCodeUpdateCacheKey) {
-  //   // Get the Task's taskChunksUrls
-  //   const taskChunksUrls = await utils.$dataCaller(
-  //     "get",
-  //     "/api/get_task_chunks_urls/" + taskMetaData.apiData.pluginKey + "/" + taskMetaData.taskKey
-  //   );
-    
-  //   // Download the task's code as a TAR file and set the new version locally
-  //   const downloadPromises = [];
-  //   const tarFiles = [];
-  //   for(let i = 0; i < taskChunksUrls.length; i++) {
-  //     const taskChunkUrl = taskChunksUrls[i];
-  //     const taskChunkPath = `${taskArchiveFolder}/${taskChunkUrl.name}.tar`;
-  //     tarFiles.push(taskChunkPath);
-  //     downloadPromises.push(utils.downloadFileToPath(taskChunkUrl.url, taskChunkPath));
-  //   }
-  //   await Promise.all(downloadPromises);
-  //   // Write tarFiles to _peth_cache.json
-  //   taskCacheData.tarFiles = tarFiles;
-  //   await utils.writeJsonFile(taskCacheFilePath, taskCacheData);
-  // }
-
-  // // Create a new folder for the task
-  // const randomKey = Math.random().toString(36).substring(2, 15) + (new Date()).getTime().toString(36);
-  // const taskTmpWorkDir = `${tasksWorkDir}/${taskMetaData.taskKey}_${randomKey}`;
-
-  // // Extract the Task code Tar files to a new destination
-  // for(let i = 0; i < taskCacheData.tarFiles.length; i++) {
-  //   const tarFile = taskCacheData.tarFiles[i];
-  //   await utils.extractTarFile(tarFile, taskTmpWorkDir);
-  // }
-
-  //TODO Merge the config from the run method with the task config folder
-
-  //TODO Create the config file `_khap_task_config.json`
-
-  //TODO Create the data file `_khap_task_data.json`
-
-  //TODO Run the task (Wait for timeout then kill the process if it's still working)
-  //TODO Use `tree-kill` to kill the process and it's children (npm install tree-kill)
-
-  // // TODO If runAndWait is used then wait for the task to finish, then get the result from `_khap_task_result.json`
-
-  const demoResult = {
-    counter: 0,
-    data: "Hello World",
-    wish: "Jannah",
-    testAPI: await utils.$dataCaller(
-      "get",
-      "/api/plugin_api/"+taskMetaData.apiData.pluginKey+"/testzz"
-      // "/api/get_task_chunks_urls/" + taskMetaData.apiData.pluginKey + "/" + taskMetaData.taskKey
-    ),
-  };
-
-  const key = "plugins-engine-task-result-of-" + taskMetaData.taskKey + "-" + taskMetaData.runId;
-
-  return {
-    success: true,
-    demoData: demoResult,
-  };
+    //   await Promise.all(downloadPromises);
+    //   // Write tarFiles to _peth_cache.json
+    //   taskCacheData.tarFiles = tarFiles;
+    //   await utils.writeJsonFile(taskCacheFilePath, taskCacheData);
+    // }
+  
+    // // Create a new folder for the task
+    // const randomKey = Math.random().toString(36).substring(2, 15) + (new Date()).getTime().toString(36);
+    // const taskTmpWorkDir = `${tasksWorkDir}/${taskMetaData.taskKey}_${randomKey}`;
+  
+    // // Extract the Task code Tar files to a new destination
+    // for(let i = 0; i < taskCacheData.tarFiles.length; i++) {
+    //   const tarFile = taskCacheData.tarFiles[i];
+    //   await utils.extractTarFile(tarFile, taskTmpWorkDir);
+    // }
+  
+    //TODO Merge the config from the run method with the task config folder
+  
+    //TODO Create the config file `_khap_task_config.json`
+  
+    //TODO Create the data file `_khap_task_data.json`
+  
+    //TODO Run the task (Wait for timeout then kill the process if it's still working)
+    //TODO Use `tree-kill` to kill the process and it's children (npm install tree-kill)
+  
+    // // TODO If runAndWait is used then wait for the task to finish, then get the result from `_khap_task_result.json`
+  
+    const demoResult = {
+      counter: 0,
+      data: "Hello World",
+      wish: "Jannah",
+      testAPI: await utils.$dataCaller(
+        "get",
+        "/api/plugin_api/"+taskMetaData.apiData.pluginKey+"/testzz"
+        // "/api/get_task_chunks_urls/" + taskMetaData.apiData.pluginKey + "/" + taskMetaData.taskKey
+      ),
+    };
+  
+    const key = "plugins-engine-task-result-of-" + taskMetaData.taskKey + "-" + taskMetaData.runId;
+  
+    return {
+      success: true,
+      demoData: demoResult,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error.message,
+      __ERROR__: "Error running task"
+    };
+  }
 };
 
 module.exports = {
