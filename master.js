d9ed758f1e7639b876d94a6adc5f8fc8710d4ec0
Khalil ben zineb <khalil.benzineb@gmail.com>
2024-11-05 18:28:50 +0100

    11/05/24

--- master.js
+++ master.js
@@ -127,11 +127,11 @@ const runTask = async (taskMetaData) => {
       data: "Hello World",
       wish: "Jannah",
       taskMetaData,
-      testAPI: (await utils.$dataCaller(
-        "get",
-        "/api/plugin_api/"+taskMetaData.apiData.pluginKey+"/testzz"
-        // "/api/get_task_chunks_urls/" + taskMetaData.apiData.pluginKey + "/" + taskMetaData.taskKey
-      )) || "Failed to call API",
+      // testAPI: (await utils.$dataCaller(
+      //   "get",
+      //   "/api/plugin_api/"+taskMetaData.apiData.pluginKey+"/testzz"
+      //   // "/api/get_task_chunks_urls/" + taskMetaData.apiData.pluginKey + "/" + taskMetaData.taskKey
+      // )) || "Failed to call API",
     };
   
     const key = "plugins-engine-task-result-of-" + taskMetaData.taskKey + "-" + taskMetaData.runId;
